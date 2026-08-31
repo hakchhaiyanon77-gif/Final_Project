daf96e515930776bf5e26a469e54ffd49cd7985b
lychin <ngethlychin@gmail.com>
2026-08-31 14:17:03 +0700

    header welcome to system

--- src_cpp/Main.cpp
+++ src_cpp/Main.cpp
@@ -2,6 +2,10 @@
 using namespace std;
 
 int main(){
-
+    while(){
+        cout << "---------------------------------" << endl;
+        cout << "|     Welcome to Bank System    |" << endl;
+        cout << "---------------------------------" << endl;
+    }
     return 0;
 }
