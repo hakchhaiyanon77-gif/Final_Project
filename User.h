b723ce384ad5266552f55ef94a79c55cb6dda426
Phoung monyrak <monyrak0166@gamil.com>
2026-09-10 10:47:58 +0700

    remove error

--- User.h
+++ User.h
@@ -2,48 +2,6 @@
 #define USER_LIBRARY
 
 // {...}
-class Person {
-private:
-    string username;
-    string password;
-    string fullName;
 
-public:
-    Person(const string& username = "",
-           const string& password = "",
-           const string& fullName = "")
-        : username(username),
-          password(password),
-          fullName(fullName) {}
-
-    virtual ~Person() = default;
-
-    const string& getUsername() const {
-        return username;
-    }
-
-    const string& getFullName() const {
-        return fullName;
-    }
-
-    const string& getPassword() const {
-        return password;
-    }
-
-    void setFullName(const string& name) {
-        fullName = name;
-    }
-
-    void setPassword(const string& newPassword) {
-        password = newPassword;
-    }
-
-    bool checkPassword(const string& input) const {
-        return password == input;
-    }
-
-    virtual string getRole() const = 0;
-    virtual void displayInfo() const = 0;
-};
 
 #endif
