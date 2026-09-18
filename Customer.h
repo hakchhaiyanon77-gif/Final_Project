af96c5c5e4dd4836dfb5c211cb145a2638301291
Phoung monyrak <monyrak0166@gamil.com>
2026-09-18 16:45:17 +0700

    Add Customer constructors

--- Customer.h
+++ Customer.h
@@ -24,6 +24,16 @@ private:
     vector<string> transactions;
 
 public:
+    Customer() {}
+
+    Customer(const string& id, const string& name, const string& phoneNumber,
+         const string& customerAddress, const string& user, const string& pass)
+    : customerId(id),
+      fullName(name),
+      phone(phoneNumber),
+      address(customerAddress),
+      username(user),
+      password(pass) {}
 };
 
 #endif
